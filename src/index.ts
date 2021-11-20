c6bcb29313733cbaa6900e811f95ee95bb23bfee
Ashley <adoel3507@gmail.com>
2021-11-20 02:51:46 +0000

    swapped to sinewave testing

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import "./Types.d.ts"
 import "./style/index.scss"
 
 import { newSlide, newSprite, initCanvasUpdate, sleep } from "./modules/canvas"
-import { main } from "./test/move_sprite";
+import { main } from "./test/sinewave";
 
 const canvas = Array.from(document.getElementsByTagName('textarea'))[0];
 const update = initCanvasUpdate(canvas);
